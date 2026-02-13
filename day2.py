68a110d385b0fec1672be3ebe9b0f7554acb68da
lakshmilakshv312-ui <lakshmilakshv312@gmail.com>
2026-02-13 10:57:10 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,30 @@
+"""#read input numbers from user
+num = input()
+print(type(num)) # checking the type of number
+print(num)"""
+
+"""# implicit type converstion
+num1 = int(input())
+val = float(input())
+print(type(num1), type(val)) # checking the type of number
+res = num1 + val
+print(res)"""
+
+"""num1 = input() # read string type number
+print(type(num1)) # checking the type of number
+res = num1 + 10 # TypeError: can only concatenate str (not "int") to str
+print(res)"""
+
+
+"""# to overcome the above problem by explicit type coverstion
+num1 = input() # read string type number
+print(type(num1)) # checking the type of number
+res = num1 + 10 # TypeError: can only concatenate str (not "int") to str
+print(res)"""
+
+"""# coverting integer to float, string and boolean
+num = 12
+print(float(num))
+print(str(num))
+print(bool(num))"""
+
