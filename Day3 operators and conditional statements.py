8689df4f42a939edfa0d76b668714dafe6cdcdd3
lakshmilakshv312-ui <lakshmilakshv312@gmail.com>
2026-02-14 22:14:50 +0530

    Add files via upload

---
+++ Day3 operators and conditional statements.py
@@ -0,0 +1,75 @@
+"""# check if number is zero
+num = int(input())
+#
+if num == 0:
+    print("number is zero")
+if num != 0:
+    print("number is not zero")
+print("program done")"""
+
+"""# check if number is zero or not
+num = int(input())
+#
+if num == 0:
+    print("number is zero")
+else:
+    print("number is not zero")
+    
+print("program done")"""
+
+"""# check if number is not equal to zero
+num = int(input())
+#
+if num != 0:
+    print("number is zero")
+else:
+    print("number is not zero")
+    
+print("program done")"""
+
+"""# if else statements 
+num1 = int(input())
+num2 = int(input())
+if num1 > num2:
+    print("num1 is big")
+else:
+    print("num2 is big")"""
+
+"""#  two if statements
+num1 = int(input())
+num2 = int(input())
+if num1 > num2:
+    print("num1 is big")
+if num2 > num1:
+    print("num2 is big")"""
+
+"""# find weather the number is even or odd
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))"""
+
+"""# find number is divisable with 5 or not
+num = int(input("Enter an integer: "))
+# Check divisibility using modulus operator
+if num % 5 == 0:
+    print(f"{num} is divisible by 5.")
+else:
+    print(f"{num} is not divisible by 5.")"""
+
+"""# check number is divisable with 5 and 3 or not
+num = int(input("Enter an integer: "))
+if num % 3 == 0 and num % 5 == 0:
+    print(f"{num} is divisible by both 3 and 5.")
+else:
+    print(f"{num} is NOT divisible by both 3 and 5.")"""
+
+"""# check number is divisable with any one number or (4 or 6) or not
+num = int(input("Enter a number: "))
+if num % 4 == 0 or num % 6 == 0:
+    print(f"{num} is divisible by 4 or 6.")
+else:
+    print(f"{num} is NOT divisible by 4 or 6.")"""
+
+   
