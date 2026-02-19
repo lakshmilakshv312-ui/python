d95603ff344cd99136ad3f63201aeea3e1927cc5
lakshmilakshv312-ui <lakshmilakshv312@gmail.com>
2026-02-19 16:25:05 +0530

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,16 @@
+"""# Iterating over a list
+for i in range(5):
+    print(i)"""
+
+"""# Using range() for numeric loops
+fruits = ["apple", "banana"]
+for fruit in fruits:
+    print(fruit)"""
+
+"""# Iterating over a string
+for char in "Python":
+    print(char)"""
+
+
+
+
