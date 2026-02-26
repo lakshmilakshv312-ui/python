25bf641844e92c4d529e133a3134644e6f55e7fc
lakshmilakshv312-ui <lakshmilakshv312@gmail.com>
2026-02-26 16:02:55 +0530

    Add files via upload

---
+++ Day 8.py
@@ -0,0 +1,33 @@
+"""#function defination
+def even_or_odd(x):
+    if x % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+#function calling
+res = even_or_odd(10)
+print(res)
+print(even_or_odd(11))"""
+
+"""#sum of list elements
+def sum_list(lst):
+    total = 0
+    for num in lst:
+        total = total + num
+    return total
+#list input
+numbers = [10, 20, 30, 40, 50]
+#function call
+result = sum_list(numbers)
+print("Sum of list elements:", result)"""
+
+"""def sum_list(lst):
+    total = 0
+    for num in range(len(numbers)):
+        total = total + num
+    return total
+#list input
+numbers = [10, 20, 30, 40, 50]
+#function call
+result = sum_list(numbers)
+print("Sum of list elements:", result)"""
